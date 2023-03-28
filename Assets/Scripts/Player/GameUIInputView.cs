ab4d6e9190dabdd91163d0eceab37eee7f7af825
Vladlen <ilusenkovladlen@gmail.com>
2023-03-28 20:37:45 +0300

    Started implementing UI Input

---
+++ Assets/Scripts/Player/GameUIInputView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class GameUIInputView : MonoBehaviour, IEntityInputSource
+    {
+        public float HorizontalDirection { get; }
+        public bool Jump { get; }
+        
+        private void Awake()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnDestroy()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        public void ResetOneTimeActions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
