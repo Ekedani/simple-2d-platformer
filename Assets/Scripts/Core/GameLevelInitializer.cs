c8beb5633463727b1fc5d1347d96c65427f242c1
Vladlen <ilusenkovladlen@gmail.com>
2023-03-28 19:49:44 +0300

    Rewrited external input using PlayerBrain

---
+++ Assets/Scripts/Core/GameLevelInitializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Player;
+using UnityEngine;
+
+namespace Core
+{
+    public class GameLevelInitializer : MonoBehaviour
+    {
+        [SerializeField] private PlayerEntity _playerEntity;
+        
+        private ExternalDevicesInputReader _externalDevicesInputReader;
+        private PlayerBrain _playerBrain;
+        private void Awake()
+        {
+            _externalDevicesInputReader = new ExternalDevicesInputReader();
+            _playerBrain = new PlayerBrain
+            (
+                _playerEntity, new List<IEntityInputSource>
+                {
+                    _externalDevicesInputReader,
+                }
+            );
+        }
+
+        private void Update()
+        {
+            _externalDevicesInputReader.OnUpdate();
+        }
+
+        private void FixedUpdate()
+        {
+            _playerBrain.OnFixedUpdate();
+        }
+    }
+}
